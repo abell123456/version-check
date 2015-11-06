b36c61e7b0be9ebc1af615cea535b6400af77692
怿航 <xiangzhong.wxz@alibaba-inc.com>
2015-11-06 21:11:25 +0800

    bug fix

--- hooks/pre-push-1.js
+++ hooks/pre-push-1.js
@@ -21,6 +21,7 @@ curVersion = JSON.parse(stripJsonComments(fs.readFileSync(packageJsonPath, 'utf-
 }*/
 
 execSync('git checkout origin/master -f');
+execSync('git pull origin origin/master:origin/master -f');
 
 masterVersion = JSON.parse(stripJsonComments(fs.readFileSync(packageJsonPath, 'utf-8'))).version;
 
