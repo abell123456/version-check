e71ab8254d4b81537564d41f6bb874ce8321c345
怿航 <xiangzhong.wxz@alibaba-inc.com>
2015-11-08 11:24:25 +0800

    code modify

--- hooks/pre-push-1.js
+++ hooks/pre-push-1.js
@@ -30,7 +30,7 @@ console.log(chalk.blue('本地: %s 分支版本号为: %s'), chalk.red(curBranch
 console.log(chalk.blue('远程: %s 分支版本号为: %s'), chalk.red('master'), chalk.red(masterVersion));
 
 if (curVersion === masterVersion) {
-    console.log(chalk.red('警告:您当前本地 %s 分支版本号与远程 master 分支版本号一致,请记得在发布前更改版本号!', curBranch));
+    console.log(chalk.red('警告:您当前本地 %s 分支版本号与远程 master 分支版本号一致,请记得在发布前更改版本号!'), curBranch);
 }
 
 execSync('git checkout ' + curBranch + ' -f');
